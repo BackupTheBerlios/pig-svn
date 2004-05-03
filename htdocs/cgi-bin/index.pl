2733c79e0ff9ece29850ecece8d10c84595515ad
avar <avar@3e8c788c-82d9-0310-b9c0-9da283c90d39>
2004-05-03 17:31:10 +0000

    Initial commit of main website index file

---
+++ htdocs/cgi-bin/index.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+print "Content-type: text/html; charset=UTF-8\n\n";
+
+print <<HEAD;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>PIG - Perl Image-gallery Generator</title>
+</head>
+<body>
+<a href="http://developer.berlios.de" title="BerliOS Developer, Fostering Open Source Development">
+	<img src="http://developer.berlios.de/bslogo.php?group_id=1727" width="124px" height="32px" border="0" alt="BerliOS Developer Logo">
+</a>
+</body>
+HEAD
