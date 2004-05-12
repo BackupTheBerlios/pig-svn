2b1602d1be5cef202c5a7c04000cf514c8f6cfb8
avar <avar@3e8c788c-82d9-0310-b9c0-9da283c90d39>
2004-05-12 10:40:02 +0000

    Made a proper testcase out of this.

--- testcases/getopts.pl
+++ testcases/getopts.pl
@@ -2,14 +2,25 @@
 
 use strict;
 use Getopt::Long;
+# Bundling decides whether or not to generate short options from the long ones
+Getopt::Long::Configure qw(no_bundling no_ignore_case gnu_compat no_require_order);
 
-$Getopt::Long::ignorecase = 0;
 
-my $tag = '';
+my $times	= undef;
+my $text	= undef;
+my $verbose	= undef;
 #my $verbose = '';
 GetOptions (
-#	'verbose' => \$verbose,
-	'tag=s' => \$tag,
+	'times=i'	=> \$times,
+	'text=s' 	=> \$text,
+	'verbose' 	=> \$verbose
 );
-#print "$verbose\n";
-print "$tag\n";
+
+for ( my $i='0' ; $i<$times; $i++ ) {
+	print "$text\n";
+	if ($verbose) {
+		print STDERR "exiting the loop\n";
+	}
+}
+
+# $ ./getopts.pl --times=2 --text loop --verbose
