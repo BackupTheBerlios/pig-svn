993fba763d4190753a96883ef33d8f92c02f9ce8
avar <avar@3e8c788c-82d9-0310-b9c0-9da283c90d39>
2004-06-17 22:49:17 +0000

    Program shaped more like the final version

--- testcases/pigtest.pl
+++ testcases/pigtest.pl
@@ -1,14 +1,16 @@
 #!/usr/bin/env perl
+# Author: Ævar Arnfjörð Bjarmason
+# Licence: GPL2 and later versions of the GPL
 
 use strict;
 use warnings;
 
+# Set the initial values for variables
 my $level	= '0';
 my @tree	= ();
 my @dir		= ();
+my $first_run	= '1';
 
-
-print "<ul>\n";
 sub gen_dir {
 	opendir(DIR, '.') || die "\tCannot opendir() '.' ($!)";
 	@dir = sort readdir(DIR);
@@ -17,12 +19,25 @@ sub gen_dir {
 	closedir(DIR);
 }
 
-&gen_dir;
-
-$tree[$level] = 0;
-
-for (@dir) {
-	print "\t"x($level+1) . "<li>$_</li>\n";
+sub gen_html {
+	# On first run open the unordered list.
+	if ($first_run) {
+		$tree[$level] = '0';
+		if ($#dir ne -1) {
+			print "<ul>\n";
+			$first_run--;
+		}
+	}
 }
+
+# Do this until $#dir in $level '0' > $#dir;
+do {
+	&gen_dir;
+	&gen_html;
+} until ()
+
+#for (@dir) {
+#	print "\t"x($level+1) . "<li>$_</li>\n";
+#}
 
 print "</ul>\n";
