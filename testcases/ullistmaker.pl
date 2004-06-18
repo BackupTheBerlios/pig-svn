fe0da8765036294414defdf756fa12e0dcb4e631
avar <avar@3e8c788c-82d9-0310-b9c0-9da283c90d39>
2004-06-18 17:48:14 +0000

    very broken

--- testcases/ullistmaker.pl
+++ testcases/ullistmaker.pl
@@ -2,52 +2,68 @@
 # Author: Ævar Arnfjörð Bjarmason
 # Licence: GPL2 and later versions of the GPL
 
-use strict;
+#use strict;
 use warnings;
 
 # Set the initial values for variables
 my $level	= '0';
 my @tree	= ();
-my @dir		= ();
-my $first_run	= '1';
-my $last_run 	= '0';
+$tree[$level]	= '0';
+my @dir	= ();
+my $break	= '0';
 
-sub gen_dir {
+do {
+	# Make a list of the current directory.
 	opendir(DIR, '.') || die "\tCannot opendir() '.' ($!)";
 	@dir = sort readdir(DIR);
 	# grep out all .* and none-directory files
 	@dir = grep { !/^\./ && -d "$_" } @dir;
 	closedir(DIR);
-}
 
-sub gen_html {
-	# On first run open the unordered list.
-	if ($first_run) {
-		$tree[$level] = '0';
-		if ($#dir ne -1) { # If the array is not empty
-			print "<ul>\n";
-			$first_run--;
+	# Is @dir empty?
+	if ($#dir == -1) { # Y
+		if ($level == 0) {
+			# Nothing to do
+			$break++;
+		} else { #N
+			print "</li>";
+			chdir ('../');
+			$level--;
+			$tree[$level]++;
 		}
+	# Does $tree[$level] equal $#dir ?
+	} elsif ($#dir != $tree[$level]) { # Y
+		print "</li>";
+		print "</ul>";
+		chdir ('../');
+		$level--;
+		$tree[$level]++;
+		# Is $level = 0
+		if ($level == 0) { # Y
+			print "</ul>";
+			$break++;
+		} else { # N
+			print "</li>";
+		}
+	} else {
+		print "<ul>";
+		print "<li>";
+		print $dir[$tree[$level]];
+		chdir $dir[$tree[$level]++];
+		$level++;
+		$tree[$level] = '-1';
 	}
+	# On first run open the unordered list.
+#	if ($first_run) {
+#		$tree[$level] = '0';
+#		if ($#dir ne -1) { # If the array is not empty
+#			print "<ul>\n";
+#			$first_run--;
+#		}
+#	}
 
-	if ($last_run) {
+} until ($break)
 
-	}
-}
-
-# Checks if we should break the loop
-sub checker {
-	if ( $level eq '0') {
-		
-	}
-	$stop++;
-}
-
-# Do this until $#dir in $level '0' > $#dir;
-do {
-	&gen_dir;
-	&gen_html;
-} until ($stop)
 
 #for (@dir) {
 #	print "\t"x($level+1) . "<li>$_</li>\n";
