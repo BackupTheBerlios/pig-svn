8cd94381f5840ceabd0cfd4b7d1587554ac849fa
avar <avar@3e8c788c-82d9-0310-b9c0-9da283c90d39>
2004-06-21 13:16:17 +0000

    updated, but broken still

--- testcases/ullistmaker.pl
+++ testcases/ullistmaker.pl
@@ -2,66 +2,78 @@
 # Author: Ævar Arnfjörð Bjarmason
 # Licence: GPL2 and later versions of the GPL
 
-#use strict;
+use strict;
 use warnings;
+use Cwd;
 
 # Set the initial values for variables
 my $level	= '0';
 my @tree	= ();
-$tree[$level]	= '0';
+$tree[$level]	= '-1';
 my @dir	= ();
 my $break	= '0';
+my $debug 	= $ARGV[0];
+
+sub debug_dir {
+	print "DEBUG: directory is now: '" . getcwd . "' \n" if ($debug);
+}
 
 do {
+	&debug_dir;
+	print "DEBUG: Beginning opendir(),readdir(),closedir()\n" if ($debug); 
 	# Make a list of the current directory.
 	opendir(DIR, '.') || die "\tCannot opendir() '.' ($!)";
 	@dir = sort readdir(DIR);
 	# grep out all .* and none-directory files
 	@dir = grep { !/^\./ && -d "$_" } @dir;
 	closedir(DIR);
+	print "DEBUG: \$#dir: '$#dir'\n" if ($debug);
+	print "DEBUG: print \"\@dir\": '".@dir."'\n" if ($debug);
+	for (@dir) {print "DEBUG: \@dir contents: '$_,'" if ($debug);}
+	print "DEBUG: Ending opendir(),readdir(),closedir()\n\n" if ($debug);
 
 	# Is @dir empty?
+	print "DEBUG: Begining if (\$#dir == 1) {...\n" if ($debug);
 	if ($#dir == -1) { # Y
 		if ($level == 0) {
-			# Nothing to do
+			print "DEBUG: \$break was == 0 '$break', setting \$break++\n" if ($debug);
 			$break++;
+			print "DEBUG: \$break is now 1; '$break'\n" if ($debug);
 		} else { #N
-			print "</li>";
+			&debug_dir;
+			chdir ('../');
+			&debug_dir;
+			print "DEBUG: Decreasing \$level ($level)\n" if ($debug);
+			$level--;
+			print "DEBUG: Decreased \$level ($level)\n" if ($debug);
+			print "DEBUG: Increasing \$tree[\$level] ($tree[$level])\n" if ($debug);
+			$tree[$level]++;
+			print "DEBUG: Increased \$tree[\$level] ($tree[$level])\n" if ($debug);
+		}
+	#}
+	# Does $tree[$level] equal $#dir ?
+	} elsif ($#dir == $tree[$level]) { # Y
+		if ($level == 0) {
+			print "</ul>\n";
+			$break++;
+		} else {
+			print "</li>\n";
+			print "</ul>\n";
 			chdir ('../');
 			$level--;
 			$tree[$level]++;
 		}
-	# Does $tree[$level] equal $#dir ?
-	} elsif ($#dir != $tree[$level]) { # Y
-		print "</li>";
-		print "</ul>";
-		chdir ('../');
-		$level--;
-		$tree[$level]++;
-		# Is $level = 0
-		if ($level == 0) { # Y
-			print "</ul>";
-			$break++;
-		} else { # N
-			print "</li>";
-		}
-	} else {
-		print "<ul>";
+	} 
+	# Havent finished going through everything in the current directory
+	elsif ($#dir != $tree[$level]) {
+		print "<ul>\n";
 		print "<li>";
 		print $dir[$tree[$level]];
 		chdir $dir[$tree[$level]++];
 		$level++;
 		$tree[$level] = '-1';
 	}
-	# On first run open the unordered list.
-#	if ($first_run) {
-#		$tree[$level] = '0';
-#		if ($#dir ne -1) { # If the array is not empty
-#			print "<ul>\n";
-#			$first_run--;
-#		}
-#	}
-
+	print "\n\n";
 } until ($break)
 
 
